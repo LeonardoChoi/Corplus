371974a510cdd12b8ddcec5d8132528f180b6833
unknown <leonardohchoi@gmail.com>
2023-04-14 10:34:20 -0300

    add Card component with styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,18 @@
 import React from "react";
 import tecido2 from "../img/tecido2.jpg";
+import "../index.css";
 
 function Card() {
   return (
-    <div>
-      <img src={tecido2} alt="" />
+    <div className="card-container">
+      <div className="card-content">
+        <img className="card-img" src={tecido2} alt="" />
+        <h1 className="card-fabric-name">
+          Tecido Floral Fiore Cor - 01 (Verde), 100% Algodão, Unid. 50cm x
+          1,50mt
+        </h1>
+        <h3 className="card-price">R$99.99</h3>
+      </div>
     </div>
   );
 }
