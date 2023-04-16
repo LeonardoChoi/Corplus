d251926840580b68530a47839bdcf38cc6b55abc
unknown <leonardohchoi@gmail.com>
2023-04-16 12:40:21 -0300

    started fabric component

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ function Card({ imgSrc, fabricName, price }) {
       <div className="card-content">
         <img className="card-img" src={imgSrc} alt="" />
         <h1 className="card-fabric-name">{fabricName}</h1>
-        <h3 className="card-price">{price}</h3>
+        <h3 className="card-price">R${price}</h3>
         <button className="buy-button">Comprar</button>
       </div>
     </div>
