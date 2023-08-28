3576847ba03b845e16aeebb9c47a5d48c5ad2c97
Leo Choi <leonardohchoi@gmail.com>
2023-08-28 13:44:45 -0300

    starting new version without link component and navigation from udemy course

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,24 +1,24 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+// import { useContext } from "react";
+// import NavigationContext from "../context/navigation";
 
-function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+// function Link({ to, children }) {
+//   const { navigate } = useContext(NavigationContext);
 
-  const handleClick = (event) => {
-    if (event.metaKey || event.ctrlKey) {
-      return;
-    }
-    event.preventDefault();
-    navigate(to);
-  };
+//   const handleClick = (event) => {
+//     if (event.metaKey || event.ctrlKey) {
+//       return;
+//     }
+//     event.preventDefault();
+//     navigate(to);
+//   };
 
-  return (
-    <div>
-      <a href={to} onClick={handleClick}>
-        {children}
-      </a>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <a href={to} onClick={handleClick}>
+//         {children}
+//       </a>
+//     </div>
+//   );
+// }
 
-export default Link;
+// export default Link;
--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,33 +1,33 @@
-// import React, { createContext, useState } from "react";
-// import fabricData from "../data/fabricData";
+import React, { createContext, useState } from "react";
+import fabricData from "../data/fabricData";
 
-// export const ShopContext = createContext(null);
+export const ShopContext = createContext(null);
 
-// const getDefaultCart = () => {
-//   let cart = {};
-//   for (let i = 1; i < fabricData.length + 1; i++) {
-//     cart[i] = 0;
-//   }
-//   return cart;
-// };
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; i < fabricData.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
 
-// export const ShopContextProvider = (props) => {
-//   // this context provider has all the states for the cart that we will use in out app and also all the function that we need throughout the app.
-//   const [cartItems, setCartItems] = useState(getDefaultCart());
+export const ShopContextProvider = (props) => {
+  // this context provider has all the states for the cart that we will use in out app and also all the function that we need throughout the app.
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
-//   const addToCart = (itemId) => {
-//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-//   };
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
 
-//   const removeFromCart = (itemId) => {
-//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-//   };
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
 
-//   const contextValue = { addToCart, removeFromCart, cartItems };
+  const contextValue = { addToCart, removeFromCart, cartItems };
 
-//   return (
-//     <ShopContext.Provider value={contextValue}>
-//       {props.children}
-//     </ShopContext.Provider>
-//   );
-// };
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
