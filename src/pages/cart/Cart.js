ce674b99b64d6f655c6bb96ea5143089db76bfd5
Leo Choi <leonardohchoi@gmail.com>
2023-09-01 14:17:26 -0300

    made cart component with fabricdata.map

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import fabricData from "../../data/fabricData";
 
-function Cart() {
-  return <div>Cart</div>;
+function Cart(props) {
+  const { cartItems } = useContext(ShopContext);
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your cart items</h1>
+      </div>
+      <div className="cartItems">
+        {fabricData.map((fabric) => {
+          if (cartItems[fabric.id] !== 0) {
+            return <CartItem data={fabric} />;
+          }
+        })}
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
