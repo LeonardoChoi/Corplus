a5360e9f0359ff44bdfac2cbae34cf8687b83d52
Leo Choi <leonardohchoi@gmail.com>
2023-09-18 15:29:16 -0300

    add some comments/notes about the modal

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -3,8 +3,10 @@ import "../index.css";
 function LoginModal() {
   return (
     <div>
+      {/* position absolute makes the element go to the top left corner of the closest parent that has a position other than static. Since here there is no parent with a position other than static then the element is placed in the top left corner of the entire html document */}
       <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
       <div className="absolute inset-40 p-10 bg-white">I'm a Modal</div>
+      {/* inset-0 = if the element is position absolute the element will expand to fill the closest parent elemtn with a non-static position. Since the gray background here doesnt have a parent with non-static position it expands to the entire html doc and the second elemten has an inset of 40 which makes it like a smaller square in the center of the html doc. */}
     </div>
   );
 }
