4473cb641ba4fa8150aa8fc92e360abbe1b40407
Leo Choi <leonardohchoi@gmail.com>
2023-09-22 22:28:08 -0300

    change styles in the LoginModal component

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -17,9 +17,9 @@ function LoginModal({ onClose, children, actionBar }) {
       {/* position absolute makes the element go to the top left corner of the closest parent that has a position other than static. Since here there is no parent with a position other than static then the element is placed in the top left corner of the entire html document */}
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
+      <div className="fixed inset-40 p-10 bg-white">
         <div className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">{actionBar}</div>
