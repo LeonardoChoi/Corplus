c8319b474e3971ff16ffa2fedff794c32f7e3e35
Leo Choi <leonardohchoi@gmail.com>
2023-10-03 14:50:02 -0300

    add google authentication

--- src/pages/LoginModalPage.js
+++ src/pages/LoginModalPage.js
@@ -1,16 +1,40 @@
 import LoginModal from "../components/LoginModal";
 import "../index.css";
 import React, { useState } from "react";
-import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 function LoginModalPage() {
   const [showModal, setShowModal] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const signIn ()=>{
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const handleClick = () => {
@@ -31,9 +55,21 @@ function LoginModalPage() {
   const modal = (
     <LoginModal onClose={handleClose} actionBar={actionBar}>
       <div>
-        <input type="text" placeholder="email..." onChange={(e)=> setEmail(e.target.value)}/>
-        <input type="password" placeholder="password..." onChange={(e) => setPassword(e.target.value)} />
-        <button onClick={signIn}>Sign In</button>
+        <input
+          type="text"
+          placeholder="email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={signIn}>Register</button>
+        <div>
+          <button onClick={signInWithGoogle}>Sign In With Google</button>
+          <button onClick={logout}>logout</button>
+        </div>
       </div>
     </LoginModal>
   );
