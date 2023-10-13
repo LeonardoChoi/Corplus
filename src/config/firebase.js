6f173d8e2836bb7a48f707e6dcc8717f824d6bb5
Leo Choi <leonardohchoi@gmail.com>
2023-10-13 12:40:59 -0300

    add getStorage to be able to upload files to fireStorage

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -18,6 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
